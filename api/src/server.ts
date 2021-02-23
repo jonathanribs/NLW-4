e73cd68b70ce5c1680908f41e8ab691030f0c024
jonathanribs <jonathanribers@hotmail.com>
2021-02-22 23:48:24 -0300

    Criando Primeiras Rotas

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,16 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
+// http://localhost:3333/users
+app.get("/users", (request, response) => {
+    //return response.send("Hello World - NLW04")
+    return response.json({ message: "Hello World - NLW04" })
+})
+
+app.post("/", (request, response) => {
+    // Recebeu os dados para salvar
+    return response.json({ message: "Os dados foram salvos com sucesso" })
+})
 
 app.listen(3333, () => console.log("Server is running!"))
