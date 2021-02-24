140ecefbb169ee1a2023b78ca8a69cbeb8a618ec
jonathanribs <jonathanribers@hotmail.com>
2021-02-24 14:32:40 -0300

    Criando Repository

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { getCustomRepository, getRepository } from "typeorm";
 import { User } from "../models/User";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
         const userAlreadyExists = await usersRepository.findOne({
