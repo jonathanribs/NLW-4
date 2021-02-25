fb9d54dd138cc1b69de3f9a9fde887c9b842cfcd
jonathanribs <jonathanribers@hotmail.com>
2021-02-25 01:46:20 -0300

    Configurando banco de dados especifico para o momento de teste

---
+++ api/src/__tests__/User.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../app";
+
+describe("Users", () => {
+
+    request(app).post("/users")
+        .send({
+            email: "user@example.com",
+            name: "User Example",
+        });
+
+});
